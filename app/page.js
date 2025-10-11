7734442a72eba2a1df4f342328a264b4628379fd
RakionGamer <blackrvalera@gmail.com>
2025-10-11 01:00:14 -0400

    Aumentando tiempo de recarga

--- app/page.js
+++ app/page.js
@@ -37,7 +37,7 @@ export default function Home() {
     };
 
     fetchData();
-    const interval = setInterval(fetchData, 10000);
+    const interval = setInterval(fetchData, 30000);
     return () => clearInterval(interval);
   }, []);
 
